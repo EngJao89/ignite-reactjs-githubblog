03ea92937cf65d03e8b5c454b8674dfe49d65ddd
EngJao89 <eng_joao@outlook.com>
2023-10-01 21:19:59 -0300

    feat(gb02-theme-provider): implementa provider de tema base da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>GitHub Blog</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>GitHub Blog</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+    
   )
 }
