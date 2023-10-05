f1506f5befa4079bce58e1a85813cf43122da69f
c1334690 <joao.ribeiro@capgemini.com>
2023-10-04 23:55:50 -0300

    feat(gb09-pages): implementa componente de pages

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const PageContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  gap: 2rem;
+`
